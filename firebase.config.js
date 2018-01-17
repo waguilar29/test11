e258e1a4f399b1630ff9d2eac5f827ec63c37efa
waguilar29 <33886272+waguilar29@users.noreply.github.com>
2018-01-16 23:26:55 -0800

    Initial commit

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+var config = {
+	apiKey: "AIzaSyCaMGzGugGD4utmOOWT8ZeYlKwQPPD0SVk",
+    authDomain: "test-fa0f1.firebaseapp.com",
+    databaseURL: "https://test-fa0f1.firebaseio.com",
+    projectId: "test-fa0f1",
+    storageBucket: "test-fa0f1.appspot.com",
+    messagingSenderId: "993273855679"
+};
+
+  module.exports = config;
